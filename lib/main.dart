e329f3cd537883a51f684ae85766a93d2d8feaac
manhlamabc123 <tranducmanh12092001@gmail.com>
2022-08-08 16:54:36 +0700

    Change Color

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(primarySwatch: Colors.green),
+      theme: ThemeData(primarySwatch: Colors.blue),
       home: const RootPage(),
     );
   }
