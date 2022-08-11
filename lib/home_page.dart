ea9b2052ec9bfd1d4400900760706d452417b60f
manhlamabc123 <tranducmanh12092001@gmail.com>
2022-08-11 09:01:12 +0700

    Update Home Page

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:pdp_tcg/fight_page.dart';
 import 'package:pdp_tcg/leader_board_page.dart';
 import 'package:pdp_tcg/posts_page.dart';
@@ -24,6 +25,28 @@ class _HomePageState extends State<HomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      body: pages[currentPage],
+      bottomNavigationBar: NavigationBar(
+        destinations: const [
+          NavigationDestination(
+              icon: Icon(FontAwesomeIcons.trophy), label: 'Leader Board'),
+          NavigationDestination(
+              icon: Icon(FontAwesomeIcons.newspaper), label: 'Posts'),
+          NavigationDestination(
+              icon: Icon(FontAwesomeIcons.inbox), label: 'Deck Picker'),
+          NavigationDestination(
+              icon: Icon(FontAwesomeIcons.peopleArrowsLeftRight),
+              label: 'Duel/Fight'),
+          NavigationDestination(icon: Icon(Icons.person), label: 'Profile'),
+        ],
+        onDestinationSelected: (int index) {
+          setState(() {
+            currentPage = index;
+          });
+        },
+        selectedIndex: currentPage,
+      ),
+    );
   }
 }
