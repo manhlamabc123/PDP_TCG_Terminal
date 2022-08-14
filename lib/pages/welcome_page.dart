40aedd508756df6f546bbe16e2f025d9e630f4da
manhlamabc123 <tranducmanh12092001@gmail.com>
2022-08-14 23:27:05 +0700

    Update Welcome Page

--- lib/pages/welcome_page.dart
+++ lib/pages/welcome_page.dart
@@ -9,6 +9,7 @@ class WelcomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       body: Column(
         children: [
           Expanded(
