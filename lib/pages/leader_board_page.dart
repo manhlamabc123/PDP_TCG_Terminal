8efa7a1699a4b3174a99cabf73200061a93a006e
manhlamabc123 <tranducmanh12092001@gmail.com>
2022-09-15 23:16:22 +0700

    Update Leader Broad

--- lib/pages/leader_board_page.dart
+++ lib/pages/leader_board_page.dart
@@ -1,10 +1,29 @@
+import 'package:firebase_database/firebase_database.dart';
 import 'package:flutter/material.dart';
+import 'package:pdp_tcg/classes/user.dart';
 import 'package:pdp_tcg/constants.dart';
 import 'package:pdp_tcg/lists.dart';
 import 'package:pdp_tcg/pages/user_profile/appbar_widget.dart';
 
-class LeaderBoardPage extends StatelessWidget {
+List<User> userRanking = [];
+
+class LeaderBoardPage extends StatefulWidget {
   const LeaderBoardPage({super.key});
+
+  @override
+  State<LeaderBoardPage> createState() => _LeaderBoardPageState();
+}
+
+class _LeaderBoardPageState extends State<LeaderBoardPage> {
+  @override
+  void initState() {
+    super.initState();
+    getUserRanking().then((value) {
+      setState(() {
+        userRanking = value;
+      });
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -30,7 +49,7 @@ class LeaderBoardPage extends StatelessWidget {
                   Expanded(
                     child: ListView.builder(
                       shrinkWrap: true,
-                      itemCount: 10,
+                      itemCount: userRanking.length,
                       itemBuilder: (context, index) {
                         return Padding(
                           padding: const EdgeInsets.only(
@@ -43,12 +62,22 @@ class LeaderBoardPage extends StatelessWidget {
                                   Border.all(color: kPrimaryColor, width: 4),
                             ),
                             child: ListTile(
-                              leading: SizedBox(
-                                height: double.infinity,
-                                child: iconList[0],
+                              leading: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  Text(
+                                    "${index + 1}",
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: getTextFont(index + 1),
+                                      color: getTextColor(index + 1),
+                                    ),
+                                  ),
+                                ],
                               ),
-                              title: Text("Username $index"),
-                              subtitle: Text("Rank Point: $index"),
+                              title: Text(userRanking[index].username),
+                              subtitle:
+                                  Text(userRanking[index].rankPoint.toString()),
                             ),
                           ),
                         );
@@ -65,5 +94,56 @@ class LeaderBoardPage extends StatelessWidget {
         ],
       ),
     );
+  }
+
+  Future<List<User>> getUserRanking() async {
+    List<User> users = [];
+    final ref = FirebaseDatabase.instance.ref();
+    final snapshot = await ref.child('User').get();
+    if (snapshot.exists) {
+      Map map = snapshot.value as dynamic;
+      map.forEach((key, value) {
+        User user = User(
+          value['username'],
+          value['password'],
+          null,
+          value['rankPoint'],
+          value['gachaPoint'],
+        );
+        users.add(user);
+        users.sort(((a, b) {
+          return b.rankPoint!.compareTo(a.rankPoint!);
+        }));
+      });
+    } else {
+      debugPrint('Nothing');
+    }
+    return Future.value(users);
+  }
+
+  static double getTextFont(int index) {
+    switch (index) {
+      case 1:
+        return 47;
+      case 2:
+        return 42;
+      case 3:
+        return 39;
+      default:
+        return 32;
+    }
+  }
+
+  static Color getTextColor(int index) {
+    switch (index) {
+      case 1:
+        return const Color(0xFFffd700);
+      case 2:
+        return const Color(0xFFc0c0c0);
+      case 3:
+        return const Color(0xFFAD8A56);
+      default:
+        return Colors.white;
+    }
   }
 }
