c47cd5591de346e6ee31314b3b249a5ad691c2d1
manhlamabc123 <tranducmanh12092001@gmail.com>
2022-09-15 17:36:38 +0700

    Update Default Avatar

--- lib/pages/user_profile/profile_page.dart
+++ lib/pages/user_profile/profile_page.dart
@@ -19,7 +19,7 @@ class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
     const imagePath =
-        'https://images.unsplash.com/photo-1554151228-14d9def656e4?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=333&q=80';
+        'https://i0.wp.com/tleliteracy.com/wp-content/uploads/2017/02/default-avatar.png?ssl=1';
     String favoriteDeck = '';
     final categories = ['Achievements', 'Match History', 'Collection'];
     List<Widget> widgetList = [
