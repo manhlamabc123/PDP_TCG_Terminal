34292199d850fe814aa5a2b9f8bfb15e093a0020
manhlamabc123 <tranducmanh12092001@gmail.com>
2022-09-16 11:31:02 +0700

    Update Posts Page

--- lib/pages/posts/posts_page.dart
+++ lib/pages/posts/posts_page.dart
@@ -1,20 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:pdp_tcg/constants.dart';
+import 'package:pdp_tcg/pages/user_profile/appbar_widget.dart';
 
 class PostsPage extends StatelessWidget {
   const PostsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        padding: const EdgeInsets.all(10),
-        child: const Text(
-          'Coming soon....',
-          style: TextStyle(
-            color: Colors.white,
-            fontSize: 30,
+    return Scaffold(
+      appBar: buildAppBar(context, "News"),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: const Icon(Icons.add),
+      ),
+      body: Column(
+        children: [
+          Expanded(
+            child: ListView.builder(
+              shrinkWrap: true,
+              itemCount: 10,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.only(top: 10, right: 20, left: 20),
+                  child: Container(
+                    padding: const EdgeInsets.all(5),
+                    height: 300,
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(30),
+                    ),
+                    child: ListTile(
+                      title: Text(
+                        "Post name $index",
+                        style: const TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 30,
+                        ),
+                      ),
+                      subtitle: Text("Description $index"),
+                    ),
+                  ),
+                );
+              },
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
